1b9315cb581116a6e35f09bc3e017c7cbbb6ad35
TECHRITOMA Inc <ajuatah@gmail.com>
2022-07-11 12:14:01 +0100

    Add project files.

---
+++ EmptyMVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // how to past data from controller to view using viewbag dynamically
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "My Fist MVC";
+            ViewBag.LastName = "Andropov";
+            ViewBag.Address = "Molyko-Buea";
+            return View();
+        }
+    }
+}
